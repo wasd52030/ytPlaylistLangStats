63d39eb4389ca68424570fc8a586d36b7aa0709a
sobol <nppstcc1001050311@gmail.com>
2024-05-11 17:21:15 +0800

    change the method of take youtube api key

--- Program.cs
+++ Program.cs
@@ -25,34 +25,30 @@ async Task main()
     description: "youtube playlist url",
     getDefaultValue: () => "https://www.youtube.com/playlist?list=PLdx_s59BrvfXJXyoU5BHpUkZGmZL0g3Ip");
 
-    var YoutubeAPIKeyOption = new Option<string>(aliases: new string[] { "--ytkey" }, description: "youtube playlist url");
-
     // download command
     var downloadCommand = new Command(name: "download", description: "初步下載統整播放清單中的語言")
     {
-        PlaylistOption,
-        YoutubeAPIKeyOption
+        PlaylistOption
     };
     rootCommand.AddCommand(downloadCommand);
 
-    downloadCommand.SetHandler(async (PlaylistOption, YoutubeAPIKeyOption) =>
+    downloadCommand.SetHandler(async (PlaylistOption) =>
     {
-        var apiKey = YoutubeAPIKeyOption ?? config.apiKey;
+        var apiKey = Environment.GetEnvironmentVariable("YoutubeAPIKey") ?? config.apiKey;
         await CollectData.Invoke(PlaylistOption, config.apiKey);
-    }, PlaylistOption, YoutubeAPIKeyOption);
+    }, PlaylistOption);
 
     // stat command
     var statCommand = new Command(name: "stat", description: "進行統計")
     {
-        PlaylistOption,
-        YoutubeAPIKeyOption
+        PlaylistOption
     };
     rootCommand.AddCommand(statCommand);
-    statCommand.SetHandler(async (PlaylistOption, YoutubeAPIKeyOption) =>
+    statCommand.SetHandler(async (PlaylistOption) =>
     {
-        var apiKey = YoutubeAPIKeyOption ?? config.apiKey;
+        var apiKey = Environment.GetEnvironmentVariable("YoutubeAPIKey") ?? config.apiKey;;
         await DataAnalysis.Invoke(PlaylistOption, apiKey);
-    }, PlaylistOption, YoutubeAPIKeyOption);
+    }, PlaylistOption);
 
     await rootCommand.InvokeAsync(args);
 }
