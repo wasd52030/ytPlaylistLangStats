1d8b84bedc6722bf8037f3e104a8ca3ca5b6a6eb
sobel <nppstcc1001050311@gmail.com>
2025-12-14 01:07:13 +0800

    update sql

--- videosLangStat_CTE.sql
+++ videosLangStat_CTE.sql
@@ -11,19 +11,21 @@ LangStats AS (
         videos
     GROUP BY 
         lang
+),
+Presentation as (
+	SELECT 
+	    lang,
+	    quantity,
+	    percentage,
+	    0 AS sort_order
+	FROM LangStats
+	UNION ALL
+	SELECT
+	    '合計' AS lang,
+	    SUM(quantity) AS quantity,
+	    1.0 AS percentage,
+	    1 AS sort_order
+	FROM LangStats
 )
-SELECT 
-    lang, 
-    quantity, 
-    percentage 
-FROM 
-    LangStats
-
-UNION ALL
-
-SELECT 
-    '合計' AS lang, 
-    SUM(quantity) AS quantity,
-    1.0 AS percentage
-FROM 
-    LangStats
+SELECT lang, quantity, percentage from Presentation
+ORDER BY sort_order ASC, quantity DESC;
